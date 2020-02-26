b326383782d893bcaa048063677044efd5320470
dmrochon <dmrochon@loyno.edu>
2020-02-26 17:34:41 -0500

    added steps package and a Login Step Class

--- src/test/java/com/revature/pages/Homepage.java
+++ src/test/java/com/revature/pages/Homepage.java
@@ -1,6 +1,8 @@
 package com.revature.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Homepage {
@@ -10,5 +12,21 @@ public class Homepage {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	
+	//search button on landing page
+	@FindBy(className = "register")
+	public WebElement searchBtn;
+	
+	//search bar on landing page
+	@FindBy(id = "formGroupExampleInput2")
+	public WebElement landSearchBar;
+	
+	//the id for the anchor that wraps around the full name and profile pic
+	@FindBy(id = "navbarDropdown")
+	public WebElement dropdownBtn;
+	
+	@FindBy(css = "#navbarSupportedContent")
+	public WebElement toggleIcon;
+	
 	
 }
