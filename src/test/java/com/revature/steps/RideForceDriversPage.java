cfd567edfd50794035522042b7e0fe94436262f0
Sarah Dellheim <sarahdellheim.0xff@gmail.com>
2020-02-27 09:45:15 -0500

    Added 3 step files

---
+++ src/test/java/com/revature/steps/RideForceDriversPage.java
@@ -0,0 +1,28 @@
+package com.revature.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RideForceDriversPage {
+	@Given("^the user is logged in$")
+	public void the_user_is_logged_in() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the user clicks on the drivers page$")
+	public void the_user_clicks_on_the_drivers_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the user is taken to the drivers page$")
+	public void the_user_is_taken_to_the_drivers_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
---
+++ src/test/java/com/revature/steps/RideForceEditContactInfo.java
@@ -0,0 +1,52 @@
+package com.revature.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RideForceEditContactInfo {
+	@Given("^The user is on RideShare landing page$")
+	public void the_user_is_on_RideShare_landing_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The user clicks on their profile pic$")
+	public void the_user_clicks_on_their_profile_pic() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The user clicks profile from dropdown$")
+	public void the_user_clicks_profile_from_dropdown() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The user types in first name$")
+	public void the_user_types_in_first_name() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The user types in last name$")
+	public void the_user_types_in_last_name() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The user clicks save$")
+	public void the_user_clicks_save() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^The page should say updated successfully$")
+	public void the_page_should_say_updated_successfully() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
