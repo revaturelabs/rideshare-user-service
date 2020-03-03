357cea1f1a1c11d257d18a5218b2caafefb8c816
Bush Lam <bushlam28@gmail.com>
2020-03-03 15:51:11 -0500

    offices

--- src/test/java/com/revature/controllers/CarControllerMock.java
+++ src/test/java/com/revature/controllers/CarControllerMock.java
@@ -8,26 +8,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Valid;
-
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -36,110 +26,104 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.revature.entities.Car;
 import com.revature.entities.Employee;
 import com.revature.entities.Office;
-import com.revature.services.CarService;
-
-import io.swagger.v3.oas.annotations.Parameter;
 
 @WebAppConfiguration
 @EnableWebMvc
 @AutoConfigureMockMvc
-@SpringBootTest(
-    classes = CarControllerMock.class
-)
+@SpringBootTest(classes = CarControllerMock.class)
 public class CarControllerMock {
-	
+
 	@Autowired
 	MockMvc mvc;
-	
+
 	@MockBean
 	CarController cc;
-
+	    
 	@Test
 	public void getCarById() throws Exception {
-		
-		Office office1 = new Office(1, "312 Railroad St.");		
-		Employee employee1 = new Employee(1,"yo@hotmail.com", "Joe","Mack", "314-532-3145", "Bro123", "password", "123 Morgo town", true,true,false,false, office1);			
-		Car camry2015 = new Car(1,"Red", "Toyota", "Camry", 4, 2015, employee1);
-		
+
+		Office office1 = new Office(1, "312 Railroad St.");
+		Employee employee1 = new Employee(1, "yo@hotmail.com", "Joe", "Mack", "314-532-3145", "Bro123", "password",
+				"123 Morgo town", true, true, false, false, office1);
+		Car camry2015 = new Car(1, "Red", "Toyota", "Camry", 4, 2015, employee1);
+
 		Mockito.when(cc.getCarById(1)).thenReturn(camry2015);
-		
+
 		ResultActions rs = mvc.perform(get("/cars/1").contentType(MediaType.APPLICATION_JSON).content(""));
 		rs.andExpect(status().isOk());
-		System.out.println(rs.andReturn().getResponse().getContentAsString());		
+		System.out.println(rs.andReturn().getResponse().getContentAsString());
 	}
-	
+
 	@Test
 	public void getCars() {
-		
-		Office office1 = new Office(1, "312 Railroad St.");		
-		Employee employee1 = new Employee(1,"yo@hotmail.com", "Joe","Mack", "314-532-3145", "Bro123", "password", "123 Morgo town", true,true,false,false, office1);			
-		Car camry2015 = new Car(1,"Red", "Toyota", "Camry", 4, 2015, employee1);
-		
-		Office office2 = new Office(2, "315 Railroad St.");		
-		Employee employee2 = new Employee(2,"yso@hotmail.com", "Moe","Mack", "314-532-3145", "Bros123", "password", "123 Morgo town", true,true,false,false, office2);			
-		Car camry2010 = new Car(2,"Red", "Toyota", "Camry", 4, 2015, employee2);
-		
+
+		Office office1 = new Office(1, "312 Railroad St.");
+		Employee employee1 = new Employee(1, "yo@hotmail.com", "Joe", "Mack", "314-532-3145", "Bro123", "password",
+				"123 Morgo town", true, true, false, false, office1);
+		Car camry2015 = new Car(1, "Red", "Toyota", "Camry", 4, 2015, employee1);
+
+		Office office2 = new Office(2, "315 Railroad St.");
+		Employee employee2 = new Employee(2, "yso@hotmail.com", "Moe", "Mack", "314-532-3145", "Bros123", "password",
+				"123 Morgo town", true, true, false, false, office2);
+		Car camry2010 = new Car(2, "Red", "Toyota", "Camry", 4, 2015, employee2);
+
 		List<Car> cars = new ArrayList<Car>();
 		cars.add(camry2010);
 		cars.add(camry2015);
-		
+
 		Mockito.when(cc.getCars()).thenReturn(cars);
-		
+
 		ResultActions rs;
 		try {
 			rs = mvc.perform(get("/cars").contentType(MediaType.APPLICATION_JSON).content(""));
 			rs.andExpect(status().isOk());
-			System.out.println(rs.andReturn().getResponse().getContentAsString());	
+			System.out.println(rs.andReturn().getResponse().getContentAsString());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void addCar() throws JsonProcessingException {
-				
-		Office office1 = new Office(1, "312 Railroad St.");		
-		Employee employee1 = new Employee(1,"yo@hotmail.com", "Joe","Mack", "314-532-3145", "Bro123", "password", "123 Morgo town", true,true,false,false, office1);			
-		Car camry2015 = new Car(1,"Red", "Toyota", "Camry", 4, 2015, employee1);
-		
+
+		Office office1 = new Office(1, "312 Railroad St.");
+		Employee employee1 = new Employee(1, "yo@hotmail.com", "Joe", "Mack", "314-532-3145", "Bro123", "password",
+				"123 Morgo town", true, true, false, false, office1);
+		Car camry2015 = new Car(1, "Red", "Toyota", "Camry", 4, 2015, employee1);
+
 		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		String json = ow.writeValueAsString(camry2015);
 //		System.out.println(json);
 		Mockito.when(cc.addCar(camry2015)).thenReturn(camry2015);
-		
+
 		ResultActions rs;
 		try {
 			rs = mvc.perform(post("/cars").contentType(MediaType.APPLICATION_JSON).content(json));
 			rs.andExpect(status().isOk());
-			System.out.println(rs.andReturn().getResponse().getContentAsString());	
+			System.out.println(rs.andReturn().getResponse().getContentAsString());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
-	public void updateCar() throws JsonProcessingException {
-				
-		Office office1 = new Office(1, "312 Railroad St.");		
-		Employee employee1 = new Employee(1,"yo@hotmail.com", "Joe","Mack", "314-532-3145", "Bro123", "password", "123 Morgo town", true,true,false,false, office1);			
-		Car camry2015 = new Car(1,"Red", "Toyota", "Camry", 4, 2015, employee1);
-				
+	public void updateCar() throws Exception {
+
+		Office office1 = new Office(1, "312 Railroad St.");
+		Employee employee1 = new Employee(1, "yo@hotmail.com", "Joe", "Mack", "314-532-3145", "Bro123", "password",
+				"123 Morgo town", true, true, false, false, office1);
+		Car camry2015 = new Car(1, "Red", "Toyota", "Camry", 4, 2015, employee1);
+
 		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		String json = ow.writeValueAsString(camry2015);
-//		System.out.println(json);
+
 		Mockito.when(cc.updateCar(camry2015)).thenReturn(camry2015);
-		
-		ResultActions rs;
-		try {
-			rs = mvc.perform(put("/cars").contentType(MediaType.APPLICATION_JSON).content(json));
-			rs.andExpect(status().isOk());
-			System.out.println(rs.andReturn().getResponse());	
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		ResultActions rs = mvc.perform(put("/cars").contentType(MediaType.APPLICATION_JSON).content(json))
+				.andExpect(status().isOk());
+		System.out.println(rs.andReturn().getResponse().getContentAsString());
 	}
-	
+
 }
---
+++ src/test/java/com/revature/controllers/OfficeControllerMock.java
@@ -0,0 +1,116 @@
+package com.revature.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.revature.entities.Car;
+import com.revature.entities.Employee;
+import com.revature.entities.Office;
+
+@WebAppConfiguration
+@EnableWebMvc
+@AutoConfigureMockMvc
+@SpringBootTest(classes = OfficeControllerMock.class)
+public class OfficeControllerMock {
+	
+	@Autowired
+	MockMvc mvc;
+
+	@MockBean
+	OfficeController oc;
+	
+	@Test
+	public void getOfficeById() throws Exception {
+
+		Office office1 = new Office(1, "312 Railroad St.");
+
+		Mockito.when(oc.getOfficeById(1)).thenReturn(office1);
+
+		ResultActions rs = mvc.perform(get("/offices/1").contentType(MediaType.APPLICATION_JSON).content(""));
+		rs.andExpect(status().isOk());
+		System.out.println(rs.andReturn().getResponse().getContentAsString());
+	}
+
+	@Test
+	public void getOffices() {
+
+		Office office1 = new Office(1, "312 Railroad St.");
+		Office office2 = new Office(2, "315 High St.");
+
+		List<Office> offices = new ArrayList<Office>();
+		offices.add(office1);
+		offices.add(office2);
+
+		Mockito.when(oc.getOffices()).thenReturn(offices);
+
+		ResultActions rs;
+		try {
+			rs = mvc.perform(get("/offices").contentType(MediaType.APPLICATION_JSON).content(""));
+			rs.andExpect(status().isOk());
+			System.out.println(rs.andReturn().getResponse().getContentAsString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void addOffice() throws JsonProcessingException {
+
+		Office office1 = new Office(1, "312 Railroad St.");
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(office1);
+		Mockito.when(oc.addOffice(office1)).thenReturn(office1);
+		
+		ResultActions rs;
+		try {
+			rs = mvc.perform(post("/offices").contentType(MediaType.APPLICATION_JSON).content(json));
+			rs.andExpect(status().isOk());
+			System.out.println(rs.andReturn().getResponse().getContentAsString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void updateoffice() throws JsonProcessingException {
+
+		Office office1 = new Office(1, "312 Railroad St.");
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(office1);
+		Mockito.when(oc.updateOffice(office1)).thenReturn(office1);
+		
+		ResultActions rs;
+		try {
+			rs = mvc.perform(put("/offices").contentType(MediaType.APPLICATION_JSON).content(json));
+			rs.andExpect(status().isOk());
+			System.out.println(rs.andReturn().getResponse().getContentAsString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
