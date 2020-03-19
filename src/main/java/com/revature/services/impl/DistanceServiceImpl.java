648d159f6b1e3a9afa2aa2cd7a741b1f6aa4b2cb
Mitch Goshorn <msgoshorn@gmail.com>
2020-03-19 12:21:34 -0400

    Removed unused import

--- src/main/java/com/revature/services/impl/DistanceServiceImpl.java
+++ src/main/java/com/revature/services/impl/DistanceServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
