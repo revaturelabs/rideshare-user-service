8bf437c1a42b92b5de29886a6e3e3094b05c55cd
unknown <kanashimidays@gmail.com>
2020-03-25 13:43:37 -0400

    added annotation to info service

--- src/main/java/com/revature/services/impl/InfoServiceImpl.java
+++ src/main/java/com/revature/services/impl/InfoServiceImpl.java
@@ -2,11 +2,13 @@ package com.revature.services.impl;
 
 import com.revature.services.InfoService;
 import java.util.Map;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author May Love
  */
+@Service
 public class InfoServiceImpl implements InfoService {
 
     // Place key GOOGLE_MAPS_API_KEY and its value (provided on Slack) into your
